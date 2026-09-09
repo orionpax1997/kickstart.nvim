c1a5c95c87fb8e92d9dbc3981372746ee007ee54
OrionPax <orionpax.1997@outlook.com>
2026-09-09 17:38:41 +0800

    refactor(plugins): 适配 vim.pack 重写 kickstart 已有插件模块
    将 autopairs、debug、gitsigns、indent_line、lint、neo-tree 由
    lazy.nvim 的 require + setup 写法改为 vim.pack.add + 配置函数
    的形式，setup 顺序由 lua/plugins.lua 统一编排。
    
    同步更新 lua/kickstart/health.lua 中插件健康检查列表。

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,28 +1,20 @@
 -- Neo-tree is a Neovim plugin to browse the file system
--- Neo-Tree 是一个浏览文件系统的 Neovim 插件
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
--- leader [T]oggle [F]ileTree : 开关文件树
--- 文件树内部 `?` 查看帮助
+vim.pack.add {
+  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+}
 
-return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim', -- 一个 Neovim 的 Lua 工具库，NeoTree 依赖它提供一些实用函数。
-    'nvim-tree/nvim-web-devicons', -- 为文件和文件夹提供图标，虽然不是必需的，但推荐安装以增强界面。
-    'MunifTanjim/nui.nvim', -- 一个用于构建用户界面的 Lua 库，NeoTree 用它来创建其界面组件。
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
+vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+vim.keymap.set('n', '<leader>tf', '<Cmd>Neotree filesystem toggle<CR>', { desc = '[T]oggle [F]ile tree', silent = true })
+
+require('neo-tree').setup {
+  filesystem = {
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
       },
     },
   },
